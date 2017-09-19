de0f6a537dbc4afa6c059437f99a6de680cc2ac5
James Osborn <jamescarlosborn@gmail.com>
2017-09-18 20:01:47 -0700

    add looping to Javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,13 @@
 
 $(document).ready(function() {
   $("#meatForm").submit(function(event) {
-    var meat1Input = $("input#meat1").val();
-    var meat2Input = $("input#meat2").val();
-    var meat3Input = $("input#meat3").val();
-    var meat4Input = $("input#meat4").val();
+    var meats = ["meat1", "meat2", "meat3", "meat4"];
+    meats.forEach(function(meat) {
+      var userInput = $("input#" + meat).val();
+      $("." + meat).text(userInput);
+    });
 
-
-// User Interface Logic
-
-  $(".meat1").append(meat1Input);
-  $(".meat2").append(meat2Input);
-  $(".meat3").append(meat3Input);
-  $(".meat4").append(meat4Input);
-  $("#ticket").show();
+    $("#ticket").show();
 
     event.preventDefault();
   });
